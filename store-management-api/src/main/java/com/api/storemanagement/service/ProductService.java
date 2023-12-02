4a1851a86c56f7a51897cc46ed40361d26b7846f
Bianca Paun <biancapaun127@gmail.com>
2023-12-02 22:24:34 +0200

    Add ProductService class and addProduct method for add product functionality

---
+++ store-management-api/src/main/java/com/api/storemanagement/service/ProductService.java
@@ -0,0 +1,67 @@
+package com.api.storemanagement.service;
+
+import com.api.storemanagement.dto.ProductDTO;
+import com.api.storemanagement.entities.Category;
+import com.api.storemanagement.entities.Price;
+import com.api.storemanagement.entities.Product;
+import com.api.storemanagement.exceptions.ResourceAlreadyExistsException;
+import com.api.storemanagement.mapper.PriceMapper;
+import com.api.storemanagement.mapper.ProductMapper;
+import com.api.storemanagement.repositories.CategoryRepository;
+import com.api.storemanagement.repositories.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(CategoryService.class);
+
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository){
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Transactional
+    public Product addProduct(ProductDTO productDTO) {
+        logger.info("Attempting to add new product: {}", productDTO.getName());
+        checkIfProductExists(productDTO.getName());
+        Category category = fetchCategory(productDTO.getCategory().getId());
+
+        Product product = ProductMapper.toEntity(productDTO);
+        product.setCategory(category);
+        setProductPrices(productDTO, product);
+
+        return productRepository.save(product);
+    }
+
+    private void checkIfProductExists(String productName) {
+        productRepository.findByName(productName).ifPresent(p -> {
+            throw new ResourceAlreadyExistsException("Product with name " + productName + " already exists");
+        });
+    }
+
+    private Category fetchCategory(Integer categoryId) {
+        return categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new EntityNotFoundException("Category not found"));
+    }
+
+    private void setProductPrices(ProductDTO productDTO, Product product) {
+        if (productDTO.getPrices() != null && !productDTO.getPrices().isEmpty()) {
+            Set<Price> prices = productDTO.getPrices().stream()
+                    .map(PriceMapper::toEntity)
+                    .peek(price -> price.setProduct(product))
+                    .collect(Collectors.toSet());
+            product.setPrices(prices);
+        }
+    }
+
+}
