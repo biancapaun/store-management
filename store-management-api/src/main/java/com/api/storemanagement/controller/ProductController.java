9e19615cde3274198439033752f095d4279ffcbf
Bianca Paun <biancapaun127@gmail.com>
2023-12-02 23:27:20 +0200

    Implemented a new PUT endpoint in ProductController for update a price to current

--- store-management-api/src/main/java/com/api/storemanagement/controller/ProductController.java
+++ store-management-api/src/main/java/com/api/storemanagement/controller/ProductController.java
@@ -1,15 +1,16 @@
 package com.api.storemanagement.controller;
 
+import com.api.storemanagement.dto.ApiResponse;
 import com.api.storemanagement.dto.ProductDTO;
 import com.api.storemanagement.entities.Product;
 import com.api.storemanagement.mapper.ProductMapper;
 import com.api.storemanagement.service.ProductService;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -27,4 +28,19 @@ public class ProductController {
         ProductDTO addedProductDTO = ProductMapper.toDTO(addedProduct);
         return new ResponseEntity<>(addedProductDTO, HttpStatus.CREATED);
     }
+
+    @PutMapping("/{productId}/prices/{priceId}/make-current")
+    public ResponseEntity<ApiResponse> updateCurrentPrice(@PathVariable Integer productId, @PathVariable Integer priceId) {
+        Optional<Product> updatedProduct = productService.updateCurrentPrice(productId, priceId);
+
+        return updatedProduct
+                .map(product -> new ResponseEntity<>(
+                        new ApiResponse("Price updated to current successfully", null),
+                        HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(
+                        new ApiResponse("Product or Price not found", null),
+                        HttpStatus.NOT_FOUND));
+    }
+
+
 }
